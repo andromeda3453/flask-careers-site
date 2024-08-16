175b1c89beeb4e77350e97ff7ab47f367c109afb
Sikandar Saqlain <saqlain.sikander123@gmail.com>
2024-08-17 01:12:29 +0400

    Updated jobdetails page added functionality to fetch jobs from database

--- app.py
+++ app.py
@@ -1,50 +1,17 @@
 from flask import Flask, render_template
+from database import get_jobs
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Scientist',
-        'location': 'Dubai, UAE',
-        'salary': 30_000,
-        'about': 'Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies placerat arcu habitant gravida etiam praesent pulvinar tempor sollicitudin. Tincidunt suspendisse sem consectetur litora sed purus eros suscipit. Mi aenean sed rhoncus taciti, massa convallis dis. Mus vel orci conubia suscipit nibh aliquam. Tincidunt volutpat blandit sociosqu tristique eros scelerisque et rhoncus. Metus nam finibus posuere pellentesque neque; aliquet erat nascetur.',
-        'company': 'Moran Instruments Ltd'
-    },
-    {
-        'id': 2,
-        'title': 'Data Engineer',
-        'location': 'Abu Dhabi, UAE',
-        'salary': 40_000,
-        'about': 'Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies placerat arcu habitant gravida etiam praesent pulvinar tempor sollicitudin. Tincidunt suspendisse sem consectetur litora sed purus eros suscipit. Mi aenean sed rhoncus taciti, massa convallis dis. Mus vel orci conubia suscipit nibh aliquam. Tincidunt volutpat blandit sociosqu tristique eros scelerisque et rhoncus. Metus nam finibus posuere pellentesque neque; aliquet erat nascetur.',
-        'company': 'Dawson Advertising Ltd'
-    },
-    {
-        'id': 3,
-        'title': 'Data Analyst',
-        'location': 'RAK, UAE',
-        'salary': 35_000,
-        'about': 'Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies placerat arcu habitant gravida etiam praesent pulvinar tempor sollicitudin. Tincidunt suspendisse sem consectetur litora sed purus eros suscipit. Mi aenean sed rhoncus taciti, massa convallis dis. Mus vel orci conubia suscipit nibh aliquam. Tincidunt volutpat blandit sociosqu tristique eros scelerisque et rhoncus. Metus nam finibus posuere pellentesque neque; aliquet erat nascetur.',
-        'company': 'Clayton Training Ltd'
-    },
-    {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'UAQ, UAE',
-        'salary': 30_000,
-        'about': 'Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies placerat arcu habitant gravida etiam praesent pulvinar tempor sollicitudin. Tincidunt suspendisse sem consectetur litora sed purus eros suscipit. Mi aenean sed rhoncus taciti, massa convallis dis. Mus vel orci conubia suscipit nibh aliquam. Tincidunt volutpat blandit sociosqu tristique eros scelerisque et rhoncus. Metus nam finibus posuere pellentesque neque; aliquet erat nascetur.',
-        'company': 'Fleming Security Ltd'
-    },
-]
 
-
-@app.route('/job/<job_id>')
+@app.route("/job/<job_id>")
 def job_detail(job_id):
-    job = next(filter(lambda x: str(x['id']) == job_id, JOBS))
+    job = get_jobs("*", job_id)[0]
     print(job)
-    return render_template('jobdetails.html', job=job)
+    return render_template("jobdetails.html", job=job)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html', jobs=JOBS)
-
+    jobs = get_jobs(["title", "currency", "location", "salary", "id"])
+    return render_template("home.html", jobs=jobs)
--- database.py
+++ database.py
@@ -1,4 +1,34 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+import logging
+
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+try:
+    engine = create_engine(
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+except Exception:
+    logging.error("Error while trying to establish connection to database")
 
 
-engine = create_engine('postgresql://postgres.qrlfojxoazvckndnbwzj:[YOUR-PASSWORD]@aws-0-us-east-1.pooler.supabase.com:6543/postgres')
+def get_jobs(columns: list[str], id: int = None):
+    columns = ",".join(columns)
+    condition = f"Where id = {id}" if id else ""
+
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(f"select {columns} from jobs {condition}"))
+            jobs = result.all()
+            jobs = [row._asdict() for row in jobs]
+            return jobs
+    except Exception:
+        logging.error("Unable to retrieve jobs data")
+        return []
